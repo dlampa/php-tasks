eba40cccc8af77c8190d1f88be2fab51f70e83e3
altryx <lampa@altx.dev>
2020-08-12 13:58:49 -0600

    Changing over structure to include PHP includes;

---
+++ includes/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en-CA">
+
+<head>
+    <meta charset="utf-8" />
+    <meta description="A basic to-do web app using PHP and JS" />
+    <meta author="Damir Lampa" />
+    <!-- https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="css/reset.css" /> <!-- Credits in file -->
+    <link rel="stylesheet" href="css/layout.css" />
+    <link rel="stylesheet" href="css/fonts.css" />
+    <link rel="stylesheet" href="css/colours.css" />
+    <script src="https://kit.fontawesome.com/308aa213dc.js" crossorigin="anonymous"></script> <!-- Font Awesome Icons -->
+    <script src="js/todo.js" defer></script>
+
+    <title>jsTasks</title>
+</head>
